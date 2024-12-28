1ba76299083aa968e56523c7065610ec4cafe03c
fadouma ahmed <fannyahmed85@gmail.com>
2024-12-28 11:27:45 -0700

    complete

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
+const reducer = (accumulator, array) => {
+    console.log(accumulator, array)
+    let totalBatteries = 31
+   accumulator+= array.num
+   return totalBatteries
+}
+let totalBatteries = batteryBatches.reduce(reducer,0)
 
-// Code your solution here
